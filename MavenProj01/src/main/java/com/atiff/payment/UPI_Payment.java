c67abfcf5dc5f57e72edeece5cbce872803b0cb5
Atif-Md <mdatif0422@gmail.com>
2025-04-09 00:50:52 +0530

    TL created payment-B1 branch and added UPI_Payment class

---
+++ MavenProj01/src/main/java/com/atiff/payment/UPI_Payment.java
@@ -0,0 +1,10 @@
+package com.atiff.payment;
+
+public class UPI_Payment {
+	
+	public String doPayment(long phoneno, double amount) {
+		System.out.println("Payment is done");
+		return "Payment done";
+	}
+
+}
