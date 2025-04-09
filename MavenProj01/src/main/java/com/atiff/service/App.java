269bbec4e60b8d1e2b99225b3a01c9258a2121f9
Atif-Md <mdatif0422@gmail.com>
2025-04-09 23:20:15 +0530

    bug 123 is fixed

--- MavenProj01/src/main/java/com/atiff/service/App.java
+++ MavenProj01/src/main/java/com/atiff/service/App.java
@@ -18,6 +18,10 @@ public class App {
 		StringBuffer sb = new StringBuffer(name);
 		return sb.reverse().toString();
 	}
+	public int findNameLength(String name) {
+		System.out.println("App.findNameLength() bug 123 fixing is going on");
+		return name.length();
+	}
 
 	public static void main(String[] args) {
 		App app = new App();
